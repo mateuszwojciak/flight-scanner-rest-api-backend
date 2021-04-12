b633aebb52a83bb2c3a6059b3c4b6df0ede44033
Mateusz Wójciak <matwojciak@gmail.com>
2021-04-12 23:14:29 +0200

    First commit

---
+++ src/test/java/com/project/flightscanner/FlightScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flightscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
